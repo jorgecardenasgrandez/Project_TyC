f698154ba31fcaa7fca75be693c54f08d7981b30
Jorge CardenasG <jcardenasg32@example.com>
2018-10-22 11:27:06 -0500

    feature-funcionalidades generales de la clase familiaProf

---
+++ app/Familiaprofesional.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Familiaprofesional extends Model
+{
+    protected $fillable=[
+        'nombreFP'
+    ];
+}
---
+++ app/Http/Controllers/FamiliaprofesionalController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Familiaprofesional;
+use Illuminate\Http\Request;
+use App\Http\Requests\FamiliaProfesionalRequest;
+class FamiliaprofesionalController extends Controller
+{
+    function showRegistroFam(){
+        return view('familia_registrar');
+    }
+
+    function registrarFamiliaPro(FamiliaProfesionalRequest $request){
+        $familiaProf=new Familiaprofesional;
+        $familiaProf->nombreFP=$request->nombre;
+        $familiaProf->save();
+
+        return view('familia_registrar');
+    }
+}
