65dbc38d10e79bfe0e38a2e2debe39528d436094
Jorge CardenasG <jcardenasg32@example.com>
2018-11-09 21:18:42 -0500

    feature-logica para el perfil de un alumno

--- app/Alumno.php
+++ app/Alumno.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Matricula;
+use App\Distrito;
 class Alumno extends Model
 {
     protected $primaryKey ='dni';
@@ -13,5 +14,13 @@ class Alumno extends Model
     
     function matriculas(){
         return $this->hasMany(Matricula::class,'estudiante_dni','dni');
+    }
+
+    static function getAlumno($dni){
+        return Alumno::where('dni',compact('dni'))->first();
+    }
+
+    function distrito(){
+        return $this->belongsTo(Distrito::class);
     }
 }
