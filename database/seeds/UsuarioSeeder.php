859a09a0e203ea6b486d84c9a49289a1610f674d
Jorge CardenasG <jcardenasg32@example.com>
2018-11-09 21:24:55 -0500

    add-funcionalidades para el usuario alumno

--- database/seeds/UsuarioSeeder.php
+++ database/seeds/UsuarioSeeder.php
@@ -20,68 +20,64 @@ class UsuarioSeeder extends Seeder
                 'estado'=>'desconectado'
             ]
         );
-        /*
-        Usuario::create(
-        [
-            'usuario'=>'silvia123@gmail.com',
-            'password'=>'11111111',
-            
-            'usuario_type'=>'profesor'
-        ]
-        );
-
-        Usuario::create(
-        [
-            'usuario'=>'beatriz123@gmail.com',
-            'password'=>'22222222',
-            
-            'usuario_type'=>'profesor'
-        ]
-        );
-
-        Usuario::create(
-        [
-            'usuario'=>'enedina123@gmail.com',
-            'password'=>'33333333',
-  
-            'usuario_type'=>'profesor'
-        ]
-        );
-
-        Usuario::create(
-        [
-            'usuario'=>'doris123@gmail.com',
-            'password'=>'44444444',
-      
-            'usuario_type'=>'profesor'
-        ]
-        );
         
         Usuario::create(
         [
-            'usuario'=>'carlo123@gmail.com',
+            'usuario'=>'mrks.95617@gmail.com',
             'password'=>'70992942',
-    
-            'usuario_type'=>'alumno'
+            'usuario_type'=>'alumno',
+            'estado'=>'desconectado'
         ]
         );
 
         Usuario::create(
         [
-            'usuario'=>'luz123@gmail.com',
+            'usuario'=>'luz.malca10@gmail.com',
             'password'=>'70115689',
-          
-            'usuario_type'=>'alumno'
+            'usuario_type'=>'alumno',
+            'estado'=>'desconectado'
         ]
         );
 
         Usuario::create(
         [
-            'usuario'=>'carmen123@gmail.com',
+            'usuario'=>'carmenluisa-reyes@gmail.com',
             'password'=>'69122314',
-            'usuario_type'=>'alumno'
+            'usuario_type'=>'alumno',
+            'estado'=>'desconectado'
         ]
         );
-        */
+
+        Usuario::create(
+        [
+            'usuario'=>'beatriz1@gmail.com',
+            'password'=>'12345678',
+            'usuario_type'=>'profesor',
+            'estado'=>'desconectado'
+        ]
+        );
+
+        Usuario::create(
+            [
+                'usuario'=>'enedina1@gmail.com',
+                'password'=>'11111111',
+                'usuario_type'=>'profesor',
+                'estado'=>'desconectado'
+            ]
+        );
+
+        Usuario::create([
+            'usuario'=>'doris1@gmail.com',
+            'password'=>'22222222',
+            'usuario_type'=>'profesor',
+           'estado'=>'desconectado'
+        ]);
+
+        Usuario::create([
+            'usuario'=>'silvia1@gmail.com',
+            'password'=>'33333333',
+            'usuario_type'=>'profesor',
+           'estado'=>'desconectado'
+        ]);
     }
 }
