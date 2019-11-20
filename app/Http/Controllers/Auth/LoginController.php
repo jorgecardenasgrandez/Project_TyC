10dde0b6a0221435a4f293b1a3a305b86e7369bf
Jorge <jorge.meza9@unmsm.edu.pe>
2019-11-20 18:06:59 -0500

    validacion y alerta de login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,22 +1,19 @@
 <?php
-
 namespace App\Http\Controllers\Auth;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use RealRashid\SweetAlert\Facades\Alert;
 use Auth;
-
 class LoginController extends Controller
 {
-
     //use AuthenticatesUsers;
     
     //protected $redirectTo = '/home';
-
     /*public function __construct()
     {
         $this->middleware('guest')->except('logout');
     }*/
-
     function __construct(){ //MIDDLEWARE para que verifique si esta autenticado y lo redireccione donde queramos
         $this->middleware('guest',['only'=>'showLoginForm']); //solo iran al login los usuarios no autenticados
         // el guest es un namespace de app\http\middleware\RedirectIfAuthenticated
@@ -24,10 +21,9 @@ class LoginController extends Controller
     function showLoginForm(){
         return view('login');
     }
-
     function login(){
-
         //nos devuelve en un ARRAY los datos de usuario y contraseña
+        
         $credentials= $this->validate(request(),[ // tal ves no es muy importante ya que es requerido en el front
             'email'=>'email|required|string',
             'password'=>'required|string'
@@ -35,18 +31,16 @@ class LoginController extends Controller
         
         //devuelve un boolean accediando a la tabla users , si es que el usuario existe o no 
         if(Auth::attempt($credentials)){ //intentaremos iniciar la sesion del usuario
-           
         }
-        
-        //retornar hacia atras con los errores si el usuario no es correcto
-        return back()
-        ->withErrors(['email'=>trans('auth.failed')])
-        ->withInput(request(['email'])); //para pasarle al 'old' del html 
+            
+            //retornar hacia atras con los errores si el usuario no es correcto
+           //para pasarle al 'old' del html 
+            return redirect()->back()
+            ->with('msj', 'Las credenciales no son correctas')
+            ->withInput(request(['email']));
     }
-
     function logout(){
         Auth::logout();
-
         return redirect('/');
     }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,21 +3,35 @@
 
 	<title> INTRANET JG </title>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+    <link rel="stylesheet" type="text/css" href="/css/estilos-propios.css">
+
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="//malihu.github.io/custom-scrollbar/jquery.mCustomScrollbar.min.css">
+
+    <link rel="stylesheet" href="/css/custom.css">
+    <link rel="stylesheet" href="/css/custom-themes.css">
+        
+        
 	<link rel="icon" type="image/png" href="/img/cetpro.png"/>
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 
 	<link rel="stylesheet" type="text/css" href="/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="/fonts/iconic/css/material-design-iconic-font.min.css">
 
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
 
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/sweetalert2/5.3.5/sweetalert2.min.css">
 	<link rel="stylesheet" type="text/css" href="/css/util.css">
 	<link rel="stylesheet" type="text/css" href="/css/main.css">
+	
+	
+	
+	
 
 </head>
 <body>
@@ -28,27 +42,34 @@
 			<div class="wrap-login100 p-l-55 p-r-55 p-t-60 p-b-45"> 
 				<header> </header>
 				
-				@if(session()->has('flash'))
-            		<div class="alert alert-info"> {{ session('flash') }}</div>
-       			@endif
+				
+				
 				<form class="login100-form validate-form" action="{{ route('login') }}" method="POST">
 				@csrf
 					<span class="login100-form-title p-b-49" style="background-image: url(/img/cetpro.png)">
 						<br><br><br><br>
 					</span>
+                    
+                    
+                    @if(session()->has('msj'))
+				
+                    <div class="alert alert-danger" role="alert">{{ session('msj') }} </div>
+                    <br>
+                    @else
 
-					<div class="wrap-input100 validate-input m-b-23" {{ $errors->has('email') ? 'has-error' : ''}}>
+
+                    @endif
+
+                    
+					<div class="wrap-input100 validate-input m-b-23">
 						<span class="label-input100"> Correo electronico: </span>
 						<input class="input100" type="email" name="email" value="{{ old('email') }}"  placeholder="Escribe tu correo"  required>
 						<span class="focus-input100" data-symbol="&#xf206;"></span>
-				<!--		{{!! $errors->first('email','<span class="help-block">:message</span>') !!}} -->
 					</div>
-
-					<div class="wrap-input100 validate-input" {{ $errors->has('password') ? 'has-error' : ''}}>
+					<div class="wrap-input100 validate-input" >
 						<span class="label-input100"> Contraseña: </span>
 						<input class="input100" type="password" name="password" placeholder="Escribe tu contraseña" required>
 						<span class="focus-input100" data-symbol="&#xf190;"></span>
-				<!--		{{!! $errors->first('password','<span class="help-block">:message</span>') !!}} -->
 					</div>
 					
 					<div class="text-right p-t-8 p-b-31">
@@ -70,6 +91,8 @@
 			</div>
 		</div>
 	</div>
-
+    <script src="https://cdn.jsdelivr.net/sweetalert2/5.3.5/sweetalert2.min.js"></script>
+    
 </body>
+
 </html>
